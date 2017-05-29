72dda33edb8a00f73ab8678f92a4703ea0561d63
Open BaseBox.org <baseboxorg@users.noreply.github.com>
2017-05-29 20:33:33 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 docker rm -f mariadb
-docker run -d \
-  -v /data/mysql:/data \
-  -p 172.17.0.1:3306:3306 \ 
-  --name mariadb \
-  -e DB_USER=mariadbuser \
-  -e DB_PASS=mariadbpass \
-  cryptoplay/mariadb-alpine
+docker run -d -v /data/mysql:/data -p 172.17.0.1:3306:3306 -e DB_USER='mariadbuser' -e DB_PASS='mariadbpass' --name mariadb cryptoplay/mariadb-alpine
